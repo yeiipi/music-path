02c3d6d302423f56b05c153961ade8ea1c93547f
Your Name <you@example.com>
2020-08-13 19:24:40 -0500

    clase artista melo : 13/08/2020 19:24:40

--- artist_scrapper/class_artist.py
+++ artist_scrapper/class_artist.py
@@ -25,9 +25,8 @@ def read_data(varid , token):
 
 
 #recibe un string de una categoría y retorna toda la lista de la categoria   |jue ago 13 18:28:57 -05 2020|rcc
-def full_list(varid , token , string ):
+def full_list(elem , string ):
     lista = []
-    elem =  read_data(varid , token)
     for i in elem:
         lista.append(i[string])
     return(lista)
@@ -36,14 +35,15 @@ def full_list(varid , token , string ):
 #recibe el id de un artista, el token y retorna una lista por cada elemento que nos interesa   |jue ago 13 18:42:32 -05 2020|rcc
 class Related_Artists :
     def __init__(self, artist_id , token ,name, father):
+        elem =  read_data(artist_id , token)
         self.id = artist_id
-        self.followers = full_list(artist_id , token , 'followers')
-        self.images = full_list(artist_id , token , 'images')
+        self.followers = full_list(elem , 'followers')
+        self.images = full_list(elem , 'images')
         #WARNING la palabra name puede ser confundida con names   |jue ago 13 19:10:29 -05 2020|rcc
-        self.names = full_list(artist_id , token , 'name')
-        self.genres = full_list(artist_id , token , 'genres')
-        self.popularity = full_list(artist_id , token , 'popularity')
-        self.ids = full_list(artist_id , token , 'id')
+        self.names = full_list(elem , 'name')
+        self.genres = full_list(elem , 'genres')
+        self.popularity = full_list(elem , 'popularity')
+        self.ids = full_list(elem , 'id')
         self.father =  father
         self.name = str(name)
 
@@ -53,7 +53,11 @@ def test():
     varid = "06HL4z0CvFAxyc27GXpf02"
     token = "BQB2OdxgjWfnho-4lAkPMbOCNRZxxAgIyVFJuLz5FzR9YpC-CFTBHoYWA8mJHlSSb5YZk9b1WXSRosvRHdsVg_GL0xkFgslLOqmNyoQV3YDmmXL7mhrprLTu7HwBT6dSPGiObkLz55hke1FRj1RhGMNoXuWmX5w"
     art = Related_Artists(varid , token ,None, None )
-    artista1 = Related_Artists(art.ids[3] , token,  art.name[3],  art.id)
+    print(art.names)
+    shawn  = Related_Artists(art.ids[4] ,token ,  art.names[4] , art.id)
+    print(shawn.name)
+    print(shawn.names)
+
     
 
 
