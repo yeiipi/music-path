74d47a251028a94ffd0f1f20368dc199c712eed3
Ro <errecastillo@gmail.com>
2020-10-28 01:20:45 -0500

    ahora está recontramelo : 28/10/2020 01:20:45

--- spotyFind/super_dijkstra.py
+++ spotyFind/super_dijkstra.py
@@ -17,7 +17,7 @@ from os import popen
 
 path = "../relaciones/bandas_colombianas.json"
 
-if((len(argv)== 4)):
+if((len(argv)== 4) or (len(argv) == 3)):
     def get_nombre_banda(id_banda):
         for i in datos['nodes']:
             if(i['id'] == id_banda):
@@ -43,7 +43,6 @@ if((len(argv)== 4)):
     art2 = spotify.obtener_artista_nombre(artista_perdido2)
     artist2 = spotify.obtener_artista_id(art2['id'])
 
-    cantidad_temazos_por_artista = int(argv[3])
 
 
 
@@ -78,11 +77,6 @@ if((len(argv)== 4)):
     #plt.show()
 
 
-    elec = input("Desea hacer la playlist?    Y/N :")
-    elec = elec.upper()
-    if(elec != "y".upper()):
-        print("bye :)")
-        exit()
 
             #Aca toca hacer el bot que arme la playlist   |mar oct 27 23:31:27 -05 2020|
     print('\n')
@@ -102,37 +96,38 @@ if((len(argv)== 4)):
             #soup = BeautifulSoup(c , 'lxml')
             #primera_imagen = soup.find("src" )
             #print(primera_imagen)
+    if(len(argv) == 4):
+        cantidad_temazos_por_artista = int(argv[3])
+        playlist = []
+        for i in lista_nombres:
+            #print(i ,":") #esto imprime el nombre del artista
+            search = SearchVideos(i , offset = 1, mode = "json", max_results = cantidad_temazos_por_artista)
+            videos = search.result()
+            videos = json.loads(search.result())
+            for j in videos['search_result']:
+                #se pueden agarrar mas atributos como el id,canal,numero de views, duracion... y las fotos   |mié oct 28 00:39:36 -05 2020|
+                titulo_tema = j['title']
+                link_tema = j['link']
+                print(titulo_tema)
+                playlist.append(link_tema)
 
-    playlist = []
-    for i in lista_nombres:
-        #print(i ,":") #esto imprime el nombre del artista
-        search = SearchVideos(i , offset = 1, mode = "json", max_results = cantidad_temazos_por_artista)
-        videos = search.result()
-        videos = json.loads(search.result())
-        for j in videos['search_result']:
-            #se pueden agarrar mas atributos como el id,canal,numero de views, duracion... y las fotos   |mié oct 28 00:39:36 -05 2020|
-            titulo_tema = j['title']
-            link_tema = j['link']
-            print(titulo_tema)
-            playlist.append(link_tema)
+        #Ahora descargo la playlist   |mié oct 28 00:41:09 -05 2020|
+        #print(playlist)
+        #creo la carpeta para la playlist   |mié oct 28 00:48:13 -05 2020|
+        folder_name ="../playlists/playlist_from_{}_to_{}".format(argv[1].replace(" ","") , argv[2].replace(" ",""))
+        system("mkdir {}".format(folder_name))
+        cont = 1
+        for i in playlist:
+               res = popen("youtube-dl -x -i --audio-format mp3 {} -o {}/{:04d}.mp3".format(i,folder_name,cont)).read()
+               print("video {}/{} descargado".format(cont , len(playlist)))
+               cont += 1
 
-    #Ahora descargo la playlist   |mié oct 28 00:41:09 -05 2020|
-    #print(playlist)
-    #creo la carpeta para la playlist   |mié oct 28 00:48:13 -05 2020|
-    folder_name ="../playlists/playlist_from_{}_to_{}".format(argv[1].replace(" ","") , argv[2].replace(" ",""))
-    system("mkdir {}".format(folder_name))
-    cont = 1
-    for i in playlist:
-           res = popen("youtube-dl -x -i --audio-format mp3 {} -o {}/{:04d}.mp3".format(i,folder_name,cont)).read()
-           print("video {}/{} descargado".format(cont , len(playlist)))
-           cont += 1
-
-    print("\n"*5)
-    cont = 0
-    print("playlist:")
-    for i in playlist:
-        print("{} : {}".format(cont+1 , i))
-        cont += 1
+        print("\n"*5)
+        cont = 0
+        print("playlist:")
+        for i in playlist:
+            print("{} : {}".format(cont+1 , i))
+            cont += 1
 
 
 
@@ -140,7 +135,7 @@ if((len(argv)== 4)):
 
 
 else:
-    print("python3 {} <banda1> <banda2> <cantidad temazos por artista>".format(argv[0]))
+    print("python3 {} <banda1> <banda2> <cantidad temazos por artista>(opcional)".format(argv[0]))
 
 #esto dibuja el path mas corto   |mar oct 27 21:43:34 -05 2020|
 #try:
